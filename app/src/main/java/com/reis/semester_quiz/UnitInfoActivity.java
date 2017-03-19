3970011936b40d8857b86bb161eb592b64375118
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-03-20 01:14:28 +0800

    [ADD] init commit

---
+++ app/src/main/java/com/reis/semester_quiz/UnitInfoActivity.java
@@ -0,0 +1,13 @@
+package com.reis.semester_quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnitInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_info);
+    }
+}
