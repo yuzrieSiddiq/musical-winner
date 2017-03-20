7e71075db3fc4c24edc5efbe9b2a94931f9c1229
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-03-20 10:01:31 +0800

    [ADD] icons

--- app/src/main/java/com/reis/semester_quiz/MainActivity.java
+++ app/src/main/java/com/reis/semester_quiz/MainActivity.java
@@ -1,9 +1,8 @@
 package com.reis.semester_quiz;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -65,19 +64,37 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private void displaySelectedScreen(int itemId) {
+
+        //creating fragment object
+        Fragment fragment = null;
+
+        //initializing the fragment object which is selected
+        switch (itemId) {
+            case R.id.dashboard:
+                fragment = new MainFragment();
+                break;
+            case R.id.logout:
+                // logout
+                break;
+        }
+
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_menu1) {
-            // Handle the camera action
-        } else if (id == R.id.nav_menu2) {
-
-        } else if (id == R.id.nav_menu3) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
