6548aa6284737ee37906f49e5a1819c7b8e0cb86
Mohamad Yuzrie <yuyieyuzrie@gmail.com>
2017-07-03 23:12:02 +0800

    [ADD] clickable on each grid containers. Shows toast.

--- app/src/main/java/com/reis/semester_quiz/Unit/UnitActivity.java
+++ app/src/main/java/com/reis/semester_quiz/Unit/UnitActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.reis.semester_quiz.DashboardActivity;
@@ -31,19 +33,19 @@ public class UnitActivity extends AppCompatActivity {
         String unit_name = getIntent().getExtras().getString("unit_name");
         getSupportActionBar().setTitle(unit_name);
 
-        tabs = (PagerSlidingTabStrip) findViewById(R.id.dashboard_tabs);
-        tabs.setIndicatorColorResource(R.color.answerD);
-        tabs.setIndicatorHeight(8);
-        pager = (ViewPager) findViewById(R.id.dashboard_pager);
-        adapter = new UnitPagerAdapter(getSupportFragmentManager());
-
-        pager.setAdapter(adapter);
-
-        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
-                .getDisplayMetrics());
-        pager.setPageMargin(pageMargin);
-
-        tabs.setViewPager(pager);
+//        tabs = (PagerSlidingTabStrip) findViewById(R.id.dashboard_tabs);
+//        tabs.setIndicatorColorResource(R.color.answerD);
+//        tabs.setIndicatorHeight(8);
+//        pager = (ViewPager) findViewById(R.id.dashboard_pager);
+//        adapter = new UnitPagerAdapter(getSupportFragmentManager());
+//
+//        pager.setAdapter(adapter);
+//
+//        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+//                .getDisplayMetrics());
+//        pager.setPageMargin(pageMargin);
+//
+//        tabs.setViewPager(pager);
     }
 
     @Override
@@ -77,5 +79,57 @@ public class UnitActivity extends AppCompatActivity {
     public void onBackPressed() {
         Intent backIntent = new Intent(this, DashboardActivity.class);
         startActivity(backIntent);
+    }
+
+    public void navigateToUnitInfo(View view) {
+        Toast.makeText(this, "Test Unit Info Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToTeamInfo(View view) {
+        Toast.makeText(this, "Test Team Info Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToChat(View view) {
+        Toast.makeText(this, "Test Chat Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToIQuiz1(View view) {
+        Toast.makeText(this, "Test iRAT1 Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToIQuiz2(View view) {
+        Toast.makeText(this, "Test iRAT2 Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToIQuiz3(View view) {
+        Toast.makeText(this, "Test iRAT3 Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToTQuiz1(View view) {
+        Toast.makeText(this, "Test tRAT1 Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToTQuiz2(View view) {
+        Toast.makeText(this, "Test tRAT2 Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToTQuiz3(View view) {
+        Toast.makeText(this, "Test tRAT3 Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToMain(View view) {
+        Toast.makeText(this, "Test Main Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToAccount(View view) {
+        Toast.makeText(this, "Test Account Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToInbox(View view) {
+        Toast.makeText(this, "Test Inbox Button", Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigateToCalendar(View view) {
+        Toast.makeText(this, "Test Calendar Button", Toast.LENGTH_SHORT).show();
     }
 }
